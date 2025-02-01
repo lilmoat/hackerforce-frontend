74b7181c0f9fd0b415bc0ef0eb8e2f36e5b06a0a
lilmoat <betehaeni@gmail.com>
2025-02-02 01:16:24 +0900

    feat: add coming soon page

---
+++ src/app/shop/page.tsx
@@ -0,0 +1,11 @@
+import ComingSoon from "@/components/ComingSoon";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Shop() {
+  return (
+    <div className="flex">
+      <ComingSoon />
+    </div>
+  );
+}
