30e413b0497982c3ff53bb4a4301f9c89f92f611
lilmoat <betehaeni@gmail.com>
2025-02-04 00:10:54 +0900

    fix: section card

--- src/app/section/page.tsx
+++ src/app/section/page.tsx
@@ -1,8 +1,7 @@
-import Link from "next/link";
 import BackgroundImage from "@/components/BackgroundImage";
 import Icon from "@/components/Icon";
 import { SectionCardData } from "@/data/data";
-import { SectionCardProps } from "@/types/type";
+import SectionCard from "@/components/SectionCard";
 
 export default function Section() {
   return (
@@ -47,58 +46,5 @@ const ContentSection = () => (
         </p>
       </div>
     ))}
-  </div>
-);
-
-const SectionCard = ({
-  title,
-  description,
-  coursesCompleted,
-  totalCourses,
-  bgImage,
-  blur,
-}: SectionCardProps) => (
-  <div
-    className={`w-full xl:p-[100px] p-5 bg-cover bg-center bg-no-repeat flex justify-between items-center lg:gap-[100px] gap-3 ${
-      blur ? "blur-md" : ""
-    }`}
-    style={{ backgroundImage: `url(${bgImage})` }}
-  >
-    <div className="flex flex-col gap-4">
-      <Link
-        href="/course"
-        className="text-white hover:text-[#838282] duration-200 md:text-[60px] text-[40px] font-medium font-['Orbitron'] uppercase"
-      >
-        {title}
-      </Link>
-      <p className="text-[#a0a0a0] lg:text-xl xl:text-2xl text-sm font-['Inconsolata'] leading-loose w-[320px] sm:w-[400px] md:w-full">
-        {description}
-      </p>
-      <div className="flex flex-col md:flex-row gap-2">
-        <div className="flex items-center gap-2">
-          <span className="text-white text-lg font-['Inconsolata']">
-            Courses:
-          </span>
-          <div className="relative w-[150px] h-2">
-            <div className="absolute w-full h-2.5 bg-white/20 rounded-full"></div>
-            <div
-              className="absolute w-[58.59px] h-2.5 bg-[#d44245] rounded-full"
-              style={{ width: `${(coursesCompleted / totalCourses) * 100}%` }}
-            ></div>
-          </div>
-          <span className="text-[#d44245] text-lg font-['Inconsolata']">
-            {coursesCompleted}/{totalCourses}
-          </span>
-        </div>
-        <span className="text-white text-lg font-['Inconsolata']">
-          One new course since April 23
-        </span>
-      </div>
-    </div>
-    <Icon
-      name={"UpIcon"}
-      color="white"
-      className="lg:w-[80px] lg:h-[80px] w-[40px] h-[40px] text-white"
-    />
   </div>
 );
