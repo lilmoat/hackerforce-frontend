9e1cc2c5b111326b4ff66b3c5260e55e77414767
lilmoat <moatlittle@gmail.com>
2025-02-25 15:48:55 +0900

    fix: fix based on feedback

--- src/components/RotatingMap.tsx
+++ src/components/RotatingMap.tsx
@@ -4,14 +4,14 @@
 import { motion } from "framer-motion";
 
 const RotatingMap = () => (
-  <div className="-z-[40] absolute w-[983px] 3xl:w-[1700px] h-[985px] 3xl:h-[1700px] top-[50%] 3xl:top-[55%] !bg-dark  rounded-full overflow-hidden shadow-[inset_0px_0px_112px_20px_#434343,0px_0px_114px_#434343cc,0px_0px_20px_5px_#434343] [background:radial-gradient(50%_50%_at_50%_50%,rgba(24,26,27,0.6)_0%,rgba(24,26,27,0)_100%)]">
+  <div className="-z-[40] absolute w-[983px] lg:w-[1200px] lg:h-[1200px] 3xl:w-[1700px] h-[985px] 3xl:h-[1700px] top-[47%] 3xl:top-[45%] !bg-dark  rounded-full overflow-hidden shadow-[inset_0px_0px_112px_20px_#434343,0px_0px_114px_#434343cc,0px_0px_20px_5px_#434343] [background:radial-gradient(50%_50%_at_50%_50%,rgba(24,26,27,0.6)_0%,rgba(24,26,27,0)_100%)]">
     <motion.div
-      className="flex w-[1966px] h-full"
+      className="flex w-[14860px] h-full"
       animate={{ x: ["0%", "-50%"] }} // Moves left continuously
-      transition={{ duration: 35, repeat: Infinity, ease: "linear" }}
+      transition={{ duration: 180, repeat: Infinity, ease: "linear" }}
     >
       <img
-        width={7460}
+        width={14860}
         height={1000}
         style={{ objectFit: "cover" }}
         src="/WorldMap.png"
